6b3cfac84a56363d0549d12507d5a2a0106491f6
PC-coding <partapchauhan8@gmail.com>
2020-12-03 18:51:44 -0500

    editing react app look

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './algoseeker.png';
 import './App.css';
+import Navbar from './components/Header/Navbar';
+import Router from './components/Header/Router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Algo-Seeker
-        </p>
-
-      </header>
+      <BrowserRouter>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Navbar />
+          <Router />
+      </BrowserRouter>
     </div>
   );
 }
