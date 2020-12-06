073c6390802987af022b080c85c802ecfcc596c0
PC-coding <partapchauhan8@gmail.com>
2020-12-06 17:54:58 -0500

    progress on pathfinder

--- src/components/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/components/PathfindingVisualizer/PathfindingVisualizer.js
@@ -5,11 +5,31 @@ import './PathfindingVisualizer.css';
 export default class PathfindingVisualizer extends Component {
 
     render(){ 
+        const {grid, mouseIsPressed } = this.state;
+
         return(
             <div>
-                <h1>
+                <button >
+                Visualize
+                </button>
+                {/* <h1>
                     Pathfinding Visualizer
-                </h1>
+                </h1> */}
+                {/* <div className='grid'>
+                    {grid.map((row, rowIdx) => {
+                        const {row, col, isFinish, isStart, isWall} = node;
+                        return(
+                            <Node
+                            key={nodeIdx}
+                            col={col}
+                            isFinish={isFinish}
+                            isStart={isStart}>
+
+                            </Node>
+                        )
+                    }
+                    )}                    
+                </div> */}
             </div>
         )
     }
