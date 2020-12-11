dbfc476152f0be3fded2cfc296046e644ee8d5a7
PC-coding <partapchauhan8@gmail.com>
2020-12-10 23:24:56 -0500

    refactoring project

--- src/components/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/components/PathfindingVisualizer/PathfindingVisualizer.js
@@ -1,57 +1,13 @@
-import React, { Component } from 'react';
-import Node from './Node/Node';
-import './PathfindingVisualizer.css';
+import React, { useState, useEffect } from 'react';
 
-export default class PathfindingVisualizer extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            nodes: [],
-        };
-    }
-
-    componentDidMount(){
-        const nodes = [];
-        for (let row = 0; row < 15; row++) {
-            const currentRow = [];
-            for (let col = 0; col < 50; col++) {
-                const currentNode = {
-                    col,
-                    row,
-                    isStart: row === 10 && col === 5,
-                    isFinish: row === 10 && col === 45
-                };
-                currentRow.push(currentNode);
-            }
-            nodes.push(currentRow);
-        }
-        this.setState({nodes})
-    }
+const PathfindingVisualizer = () => {
+    const [Grid, setGrid] = useState([]);
     
-    render(){ 
-        const {grid, mouseIsPressed } = this.state;
-        const {nodes} = this.state;
-        return(
-            <div className='grid'>
-                {/* <button >
-                Visualize
-                </button> */}
-                {nodes.map((row, rowIdx) => {
-                    return ( 
-                    <div key={rowIdx}>
-                        {row.map((node, nodeIdx) => {
-                            const {isStart, isFinish} = node;
-                            return(
-                                <Node 
-                                key={nodeIdx} 
-                                isStart={isStart}
-                                isFinish={isFinish}></Node>
-                            );
-                        })}
-                    </div>
-                    );
-                })}                 
-            </div>
-        )
-    }
+    return(
+        <div>
+            <h1> Pathfinding Visualizer</h1>
+        </div>
+    )
 }
+
+export default PathfindingVisualizer;
