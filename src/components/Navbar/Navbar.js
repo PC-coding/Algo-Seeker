472a5789091656ff01d544c4893c42a40a1d3b9f
PC-coding <partapchauhan8@gmail.com>
2020-12-13 17:54:27 -0500

    navbar styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,29 +6,31 @@ import './Navbar.css'
 const Navbar = () => {
     return(
         <nav>
-            <div className='navone'>
-                <Link to='/' style={{padding: 20}}>
-                    <img src={img} style={{height:'50px', width:'50px'}} />
-                </Link>
-                <Link to='/' style={{textDecoration:'None'}}> 
-                Choose your Algorithm!: </Link>
+                <div>
+                    <Link to='/' style={{padding: 20}}>
+                        <img src={img} style={{height:'50px', width:'50px'}} />
+                    </Link>
+                </div>
+                <div className='nav1'>
+                    <Link to='/' style={{ padding: 20, textDecoration:'None', color:'white', fontWeight:'bold', fontSize: 'large'}}> 
+                    Choose your Algorithm!: </Link>
 
-                <Link to='/dijkstras' style={{padding: 20}}>
-                Djikstra's
-                </Link>
+                    <Link to='/dijkstras' style={{padding: 20, color:'white', fontWeight:'bold', fontSize: 'large'}}>
+                    Djikstra's
+                    </Link>
 
-                <Link to='/bfs' style={{padding: 20}}>
-                Breadth-First Search
-                </Link>
+                    <Link to='/bfs' style={{padding: 20, color:'white', fontWeight:'bold', fontSize: 'large'}}>
+                    Breadth-First Search
+                    </Link>
 
-                <Link to='/bsort' style={{padding: 20}}>
-                Bubble Sort
-                </Link>
+                    <Link to='/bsort' style={{padding: 20, color:'white', fontWeight:'bold', fontSize: 'large'}}>
+                    Bubble Sort
+                    </Link>
 
-                <Link to='/qsort' style={{padding: 20}}>
-                Quick Sort
-                </Link>
-            </div>
+                    <Link to='/qsort' style={{padding: 20, color:'white', fontWeight:'bold', fontSize: 'large'}}>
+                    Quick Sort
+                    </Link>
+                </div>
         </nav>
     )
 }
