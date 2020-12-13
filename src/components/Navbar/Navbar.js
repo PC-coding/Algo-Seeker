40e1f07c8f8da1910bbb9de9470ebd480984ae78
PC-coding <partapchauhan8@gmail.com>
2020-12-13 15:11:11 -0500

    making navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(){
     return(
         <nav>
-            
+            <Link to='/djikstras'>
+            Djikstra's
+            </Link>
+
+            <Link to='/bfs'>
+            Breadth-First Search
+            </Link>
+
+            <Link to='/bsort'>
+            Bubble Sort
+            </Link>
+
+            <Link to='/qsort'>
+            Quick Sort
+            </Link>
         </nav>
     )
 }
