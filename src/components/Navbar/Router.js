edf4d5d374e11c2bc5fd2de3ccd7caff7afb76ef
PC-coding <partapchauhan8@gmail.com>
2020-12-13 00:56:19 -0500

    editing router

--- src/components/Navbar/Router.js
+++ src/components/Navbar/Router.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import PathfindingVisualizer from '../PathfindingVisualizer/PathfindingVisualizer';
+import QuickSort from '../QuickSort/QuickSort';
+import BubbleSort from '../BubbleSort/BubbleSort';
+import BFS from '../BFS/BFS';
+import BFS2 from '../BFS/BFS2';
 
 export default function Router(){
     return(
         <BrowserRouter>
             <Route>
-                
+
             </Route>
         </BrowserRouter>
     )
