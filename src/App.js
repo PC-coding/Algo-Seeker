f570f8093a7ef2f07148758c1b65973028b94fe4
PC-coding <partapchauhan8@gmail.com>
2020-12-13 15:29:02 -0500

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import logo from './algoseeker.png';
 import './App.css';
-import { BrowserRouter, Link } from 'react-router-dom';
-import PathfindingVisualizer from './components/PathfindingVisualizer/PathfindingVisualizer';
-import BFS from './components/BFS/BFS';
-import BFS2 from './components/BFS/BFS2';
-import QuickSort from './components/QuickSort/QuickSort';
-import BubbleSort from './components/BubbleSort/BubbleSort';
+import { BrowserRouter } from 'react-router-dom';
 import Router from './components/Navbar/Router';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
 
   return (
-        <BrowserRouter>
+    <BrowserRouter>
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Pathfinding Visualizer</h1>
         <header>
-
-            <Navbar />
-            <Router />
+          <Navbar />
+          <Router />
         </header>
-            {/* <PathfindingVisualizer />
-            <BFS2 />
-            <BFS />
-            <BubbleSort />
-          <QuickSort /> */}
       </div>
-          </BrowserRouter>
-    )
+    </BrowserRouter>
+  )
 }  
 
   
