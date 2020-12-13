1033e77e0b75dc58bc8317fd1f58eb00be20919d
PC-coding <partapchauhan8@gmail.com>
2020-12-13 00:53:24 -0500

    creating router

---
+++ src/components/Navbar/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+export default function Router(){
+    return(
+        <BrowserRouter>
+            <Route>
+                
+            </Route>
+        </BrowserRouter>
+    )
+}
