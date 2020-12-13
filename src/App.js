d266c59b4467e040e7085237fa4ffe527863ad1e
PC-coding <partapchauhan8@gmail.com>
2020-12-13 15:37:55 -0500

    fixing navbar styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './algoseeker.png';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './components/Navbar/Router';
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
         <header>
           <Navbar />
           <Router />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../algoseeker.png'
+import './Navbar.css'
 
 const Navbar = () => {
     return(
         <nav>
-            <div>
-                <Link to='/'>
-                    <img src={img} />
+            <div className='navone'>
+                <Link to='/' style={{padding: 20}}>
+                    <img src={img} style={{height:'50px', width:'50px'}} />
                 </Link>
 
-                <Link to='/dijkstras'>
+                <Link to='/dijkstras' style={{padding: 20}}>
                 Djikstra's
                 </Link>
 
-                <Link to='/bfs'>
+                <Link to='/bfs' style={{padding: 20}}>
                 Breadth-First Search
                 </Link>
 
-                <Link to='/bsort'>
+                <Link to='/bsort' style={{padding: 20}}>
                 Bubble Sort
                 </Link>
 
-                <Link to='/qsort'>
+                <Link to='/qsort' style={{padding: 20}}>
                 Quick Sort
                 </Link>
             </div>
