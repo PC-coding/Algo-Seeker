d8308bb0ca425bfc0e5a0b465b1db6dbbf276896
PC-coding <partapchauhan8@gmail.com>
2020-12-14 23:08:12 -0500

    making homepage for app

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function HomePage(){
+    return(
+        <div>
+            <img src={} />
+            <img src={} />
+            <img src={} />
+            <img src={} />
+            <img src={} />
+            <img src={} />
+        </div>
+    )
+}
