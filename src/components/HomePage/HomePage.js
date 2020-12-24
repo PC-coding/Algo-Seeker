436d84c1f2456f63402a67f323642758185d889a
PC-coding <partapchauhan8@gmail.com>
2020-12-23 22:37:59 -0500

    animation added, styling in progress

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,39 +6,122 @@ import qsort from '../../img/qsort.png';
 import ts_genetic from '../../img/ts_genetic.png';
 import astar from '../../img/astar.png';
 import { Link } from 'react-router-dom';
+import Particles from 'react-tsparticles';
 
 
 export default function HomePage(){
     return(
         <div>
-            <h1 style={{textAlign:'center', fontSize:'5rem'}}>Algo-Seeker</h1>
-           <div style={{display:'grid', gridTemplateColumns: "repeat(3, 1fr)", 
-                    gridGap: 20, alignContent:'center', marginLeft:'180px', 
-                    marginTop: '25px'}}>
-            <Link to='/bfs'>
-                <img src={BFS} />
-            </Link>
+                    {/* <h1 style={{textAlign:'center', fontSize:'5rem'}}>Algo-Seeker</h1>
+                    <div style={{display:'grid', gridTemplateColumns: "repeat(3, 1fr)", 
+                                gridGap: 20, alignContent:'center', marginLeft:'180px', 
+                                marginTop: '25px'}}>
+                        <Link to='/bfs'>
+                            <img src={BFS} />
+                        </Link>
 
-            <Link to='/dijkstras'>
-                <img src={dijkstra} />
-            </Link>
+                        <Link to='/dijkstras'>
+                            <img src={dijkstra} />
+                        </Link>
 
-            <Link to='/bsort'>
-                <img src={bsort} />
-            </Link>
+                        <Link to='/bsort'>
+                            <img src={bsort} />
+                        </Link>
 
-            <Link to='/qsort'>
-                <img src={qsort} />
-            </Link>
+                        <Link to='/qsort'>
+                            <img src={qsort} />
+                        </Link>
 
-            <Link to='/travelingsalesperson'>
-                <img src={ts_genetic} />
-            </Link>
+                        <Link to='/travelingsalesperson'>
+                            <img src={ts_genetic} />
+                        </Link>
 
-            <Link to='/astar'>
-                <img src={astar} />
-            </Link>
-        </div> 
+                        <Link to='/astar'>
+                            <img src={astar} />
+                        </Link>
+                    </div>  */}
+        <Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#0d47a1",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    );
         </div>
         
     )
